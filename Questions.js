fa2403bcfad506eb2dc810725c05f28de3fb50d6
Dominador G de la Cruz Jr <domenikos858@gmail.com>
2020-03-29 23:13:29 -0700

    initial

---
+++ Questions.js
@@ -0,0 +1,27 @@
+var Questions = [
+    {
+        title: "Which is NOT an example of a HTML Event?",
+        choices: ["An HTML web page that has finished loading.", "An HTML input field was changed.", "An HTML button was clicked.", "None of the above."],
+        answer: "None of the above."
+    },
+    {
+        title: "The real strength of JavaScript arrays are the built-in array properties and methods.",
+        choices: ["True", "False"],
+        answer: "True"
+    },
+    {
+        title: "x>>=y is same as:",
+        choices: ["x>=>y", "x=>>y", "x=x>>y", "x=x>>>y"],
+        answer: "x=x>>y"
+    },
+    {
+        title: "Unlike in Algebra, the equal sign (=) in JavaScript is an 'assignment' operator; not and 'equal to' operator.",
+        choices: ["True", "False"],
+        answer: "True"
+    },
+    {
+        title: "In CSS, this is used to generate space around the elemen't content, inside of any defined borders.",
+        choices: ["margin", "outer border", "padding", "inner border"],
+        answer: "padding"
+    },
+  ];
